96955a06908b0c561486753c5e5306ce3db33431
eSam0531 <esama01@jaguar.tamu.edu>
2022-04-26 15:49:59 -0500

    load balancer for prod web

--- prod.tf
+++ prod.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   profile = "default"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
@@ -9,13 +9,29 @@ resource "aws_s3_bucket" "prod_tf_course" {
 
 resource "aws_default_vpc" "default" {}
 
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+  
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-east-1b"
+  
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
 resource "aws_security_group" "prod_web" {
   name        = "prod_web"
   description = "Allow standard http and https ports inbound and everything outbound"
 
   ingress {
     from_port   = 80
-    to_port	= 80
+    to_port	    = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -26,9 +42,9 @@ resource "aws_security_group" "prod_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -40,7 +56,7 @@ resource "aws_security_group" "prod_web" {
 resource "aws_instance" "prod_web" {
   count = 2
   
-  ami         	= "ami-0b0ea68c435eb488d"
+  ami         	= "ami-03bd6eac7ee70dde9"
   instance_type = "t2.nano"
   
   vpc_security_group_ids = [
@@ -58,6 +74,24 @@ resource "aws_eip_association" "prod_web" {
 } 
 
 resource "aws_eip" "prod_web" {  
+  tags = {
+    "Terraform" : "true"
+  }
+}
+
+resource "aws_elb" "prod_web" {
+  name            = "prod-web"
+  instances       = aws_instance.prod_web.*.id
+  subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  security_groups = [aws_security_group.prod_web.id]
+  
+  listener {
+	instance_port     = 80
+	instance_protocol = "http"
+	lb_port           = 80
+	lb_protocol       = "http"
+  }
+  
   tags = {
     "Terraform" : "true"
   }
